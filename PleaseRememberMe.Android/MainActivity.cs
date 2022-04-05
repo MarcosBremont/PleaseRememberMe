d39061a9f501702608c2dd2aff1ffe77480870dd
Marcos Bremont <bremont123@hotmail.com>
2022-04-05 15:18:46 -0400

    Version 1.0

--- PleaseRememberMe.Android/MainActivity.cs
+++ PleaseRememberMe.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace PleaseRememberMe.Droid
 {
-    [Activity(Label = "PleaseRememberMe", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "PleaseRememberMe", Icon = "@mipmap/PleaseRememberMe", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
